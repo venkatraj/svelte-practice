d930b6eae64434674fdf0f57368afc4dda39d48a
Venkat Raj <venkat@codinggeek.com>
2019-06-07 12:33:18 +0530

    rendering html in a string in component

--- learning/src/App.svelte
+++ learning/src/App.svelte
@@ -3,6 +3,7 @@
 	let name = 'world';
 	let who = 'A man';
 	let src = 'https://svelte.dev/tutorial/image.gif';
+	let string = `this string contains some <strong>HTML!!!</strong> and will not be escaped by svelte by default. Be sure to manually escape code from untrusted sources or risk XSS`
 </script>
 
 <style>
@@ -17,3 +18,4 @@
 <img {src} alt="{who} dancing" />
 <p>This is a paragraph with component scoped styles</p>
 <Nested />
+<p>{@html string}</p>
