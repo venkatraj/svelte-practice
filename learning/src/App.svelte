cc7ba97936ef4473ae1829667608d1db532158a3
Venkat Raj <venkat@codinggeek.com>
2019-06-07 12:25:58 +0530

    component scoped styles

--- learning/src/App.svelte
+++ learning/src/App.svelte
@@ -4,5 +4,14 @@
 	let src = 'https://svelte.dev/tutorial/image.gif';
 </script>
 
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS';
+		font-size: 2em;
+	}
+</style>
+
 <h1>Hello {name}!</h1>
 <img {src} alt="{who} dancing" />
+<p>This is a paragraph with component scoped styles</p>
